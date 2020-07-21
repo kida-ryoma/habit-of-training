fae1b9c43ae544b85f473b012bc09ffbdf8f5af0
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-07-21 11:36:07 +0900

    add training_menu#show page

--- app/controllers/training_menus_controller.rb
+++ app/controllers/training_menus_controller.rb
@@ -1,10 +1,24 @@
 class TrainingMenusController < ApplicationController
   require "date"
+  before_action :get_menu, only: [:new, :show]
 
   def index
   end
 
   def new
+    now = Time.now
+    today = now.wday + 1
+    @training_menu = TrainingMenu.find(today).menu
+  end
+
+  def show
+    now = Time.now
+    today = now.wday + 1
+    @training_menu = TrainingMenu.find(today).menu
+  end
+
+  private
+  def get_menu
     now = Time.now
     today = now.wday + 1
     @training_menu = TrainingMenu.find(today).menu
